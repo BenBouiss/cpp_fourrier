a1ae64bee2cc69a9da35cfb1ec3b8dda25e621f4
Benjamin BOUISSOU <benjamin.bouissou@gmail.com>
2024-05-14 10:55:36 +0200

    .

--- src/sound_generation.cpp
+++ src/sound_generation.cpp
@@ -8,6 +8,6 @@ std::vector<float> additive_synthese(int scintillance, int buffer_size){
     return {0.0};
 }
 
-std::vector<float> soustractive_synthese(int scintillance, int buffer_size){
-    return {0.0};
+std::vector<float> soustractive_synthese(std::vector<float> initial_sound, int scintillance, int buffer_size){
+
 }
--- src/sound_generation.h
+++ src/sound_generation.h
@@ -7,5 +7,5 @@
 
 std::vector<float> additive_synthese(int scintillance, int buffer_size);
 
-std::vector<float> soustractive_synthese(int scintillance, int buffer_size);
+std::vector<float> soustractive_synthese(std::vector<float> initial_sound, int scintillance, int buffer_size);
 #endif
