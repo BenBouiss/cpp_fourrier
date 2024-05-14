9c1ba2fa2cdae01dda8da0d033ff70bd840b229e
Sylviagit <sylviaglehmann@outlook.fr>
2024-05-14 14:03:31 +0200

    readme + stop in setup + calcul frequncy

--- src/utils.cpp
+++ src/utils.cpp
@@ -35,3 +35,9 @@ void print_array_float(std::vector<float> vect){
 	std::cout << "\n" << std::endl ;
 }
 
+
+// Function that convert pitch into frequency. Pitch is equal to noctave * 12 + note
+// for example for A4 =
+float pitchToFrequency(int pitch, float A4frequency = 440.f, int A4pitch = 57){
+	return A4frequency * pow(2, ((pitch - A4pitch) / 12.f));
+}
